2c6b0f5a5a82bb8b6fa02867515c35aa23e0ece9
robmikh <rob.mikh@outlook.com>
2022-06-17 11:55:20 -0700

    Add project files.

---
+++ UWPCaptureSample/MainPage.xaml.cs
@@ -0,0 +1,228 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Numerics;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.Graphics.Capture;
+using Windows.Graphics.Display;
+using Windows.Security.Authorization.AppCapabilityAccess;
+using Windows.UI.Composition;
+using Windows.UI.Popups;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+
+namespace UWPCaptureSample
+{
+    public sealed partial class MainPage : Page
+    {
+        private Compositor _compositor;
+        private SpriteVisual _root;
+        private SpriteVisual _content;
+        private CompositionSurfaceBrush _brush;
+
+        private CanvasDevice _device;
+        private SimpleCapture _capture;
+
+        private bool _isProgrammaticPresent = false;
+        private bool _isBorderlessPresent = false;
+        private bool _isCusrorCapturePresent = false;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var ignored = InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            InitializeComposition();
+            UpdateFeaturePresence();
+            if (_isProgrammaticPresent)
+            {
+                await RequestAccessAsync();
+                RefreshProgrammaticComboBoxes();
+            }
+            else
+            {
+                WindowsComboBox.IsEnabled = false;
+                DisplaysComboBox.IsEnabled = false;
+                RefreshButton.IsEnabled = false;
+            }
+            if (!_isBorderlessPresent)
+            {
+                BorderCheckBox.IsEnabled = false;
+            }
+            if (!_isCusrorCapturePresent)
+            {
+                CursorCheckBox.IsEnabled = false;
+            }
+        }
+
+        private void UpdateFeaturePresence()
+        {
+            _isProgrammaticPresent = ApiInformation.IsMethodPresent(typeof(GraphicsCaptureItem).FullName, nameof(GraphicsCaptureItem.TryCreateFromWindowId));
+            _isBorderlessPresent = ApiInformation.IsPropertyPresent(typeof(GraphicsCaptureSession).FullName, nameof(GraphicsCaptureSession.IsBorderRequired));
+            _isCusrorCapturePresent = ApiInformation.IsPropertyPresent(typeof(GraphicsCaptureSession).FullName, nameof(GraphicsCaptureSession.IsCursorCaptureEnabled));
+        }
+
+        private void InitializeComposition()
+        {
+            _compositor = Window.Current.Compositor;
+            _root = _compositor.CreateSpriteVisual();
+            _content = _compositor.CreateSpriteVisual();
+            _brush = _compositor.CreateSurfaceBrush();
+
+            _root.RelativeSizeAdjustment = Vector2.One;
+            _content.AnchorPoint = new Vector2(0.5f, 0.5f);
+            _content.RelativeOffsetAdjustment = new Vector3(0.5f, 0.5f, 0);
+            _content.RelativeSizeAdjustment = Vector2.One;
+            _content.Size = new Vector2(-80, -80);
+            _content.Brush = _brush;
+            _brush.HorizontalAlignmentRatio = 0.5f;
+            _brush.VerticalAlignmentRatio = 0.5f;
+            _brush.Stretch = CompositionStretch.Uniform;
+            var shadow = _compositor.CreateDropShadow();
+            shadow.Mask = _brush;
+            _content.Shadow = shadow;
+            _root.Children.InsertAtTop(_content);
+
+            _device = new CanvasDevice();
+            ElementCompositionPreview.SetElementChildVisual(VisualGrid, _root);
+        }
+
+        private async Task RequestAccessAsync()
+        {
+            var accessResult = await GraphicsCaptureAccess.RequestAccessAsync(GraphicsCaptureAccessKind.Programmatic);
+            if (accessResult != AppCapabilityAccessStatus.Allowed)
+            {
+                var dialog = new MessageDialog($"Programmatic access denied! Result: {accessResult.ToString()}");
+                await dialog.ShowAsync();
+            }
+
+            accessResult = await GraphicsCaptureAccess.RequestAccessAsync(GraphicsCaptureAccessKind.Borderless);
+            if (accessResult != AppCapabilityAccessStatus.Allowed)
+            {
+                var dialog = new MessageDialog($"Borderless access denied! Result: {accessResult.ToString()}");
+                await dialog.ShowAsync();
+            }
+        }
+
+        private void RefreshProgrammaticComboBoxes()
+        {
+            var windowList = new List<GraphicsCaptureItem>();
+            var windows = WindowServices.FindAllTopLevelWindowIds();
+            foreach (var window in windows)
+            {
+                var item = GraphicsCaptureItem.TryCreateFromWindowId(window);
+                if (item != null)
+                {
+                    windowList.Add(item);
+                }
+            }
+            WindowsComboBox.ItemsSource = windowList;
+
+            var displayList = new List<GraphicsCaptureItem>();
+            var displays = DisplayServices.FindAll();
+            foreach (var display in displays)
+            {
+                var item = GraphicsCaptureItem.TryCreateFromDisplayId(display);
+                if (item != null)
+                {
+                    displayList.Add(item);
+                }
+            }
+            DisplaysComboBox.ItemsSource = displayList;
+        }
+
+        private void StartCapture(GraphicsCaptureItem item)
+        {
+            _capture = new SimpleCapture(_device, item);
+            BorderCheckBox.IsChecked = true;
+            CursorCheckBox.IsChecked = true;
+
+            var surface = _capture.CreateSurface(_compositor);
+            _brush.Surface = surface;
+
+            _capture.StartCapture();
+        }
+
+        private void StopCapture()
+        {
+            _capture?.Dispose();
+            _brush.Surface = null;
+        }
+
+        private async void OpenPickerButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new GraphicsCapturePicker();
+            var item = await picker.PickSingleItemAsync();
+            if (item != null)
+            {
+                StartCapture(item);
+            }
+        }
+
+        private void StopCaptureButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopCapture();
+        }
+
+        private void ProgrammaticComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = (ComboBox)sender;
+            if (comboBox.SelectedItem == null)
+            {
+                return;
+            }
+
+            var item = (GraphicsCaptureItem)comboBox.SelectedItem;
+            StartCapture(item);
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshProgrammaticComboBoxes();
+        }
+
+        private void BorderCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            SetBorderRequired(true);
+        }
+
+        private void BorderCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SetBorderRequired(false);
+        }
+
+        private void SetBorderRequired(bool isRequired)
+        {
+            if (_isBorderlessPresent)
+            {
+                _capture?.SetBorderRequired(isRequired);
+            }
+        }
+
+        private void CursorCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            SetCursorCaptureEnabled(true);
+        }
+
+        private void CursorCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SetCursorCaptureEnabled(false);
+        }
+
+        private void SetCursorCaptureEnabled(bool isEnabled)
+        {
+            if (_isCusrorCapturePresent)
+            {
+                _capture?.SetIsCursorCaptureEnabled(isEnabled);
+            }
+        }
+    }
+}
---
+++ UWPCaptureSample/SimpleCapture.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.Composition;
+
+namespace UWPCaptureSample
+{
+    class SimpleCapture : IDisposable
+    {
+        public SimpleCapture(CanvasDevice device, GraphicsCaptureItem item)
+        {
+            _item = item;
+            _device = device;
+            _closeLock = new object();
+
+            // TODO: Dpi?
+            _swapChain = new CanvasSwapChain(_device, item.Size.Width, item.Size.Height, 96);
+
+            _framePool = Direct3D11CaptureFramePool.CreateFreeThreaded(
+                    _device,
+                    DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                    2,
+                    item.Size);
+            _session = _framePool.CreateCaptureSession(item);
+            _lastSize = item.Size;
+
+            _framePool.FrameArrived += OnFrameArrived;
+        }
+
+        public void SetIsCursorCaptureEnabled(bool isEnabled)
+        {
+            _session.IsCursorCaptureEnabled = isEnabled;
+        }
+
+        public void SetBorderRequired(bool isRequired)
+        {
+            _session.IsBorderRequired = isRequired;
+        }
+
+        public void StartCapture()
+        {
+            _session.StartCapture();
+        }
+
+        public ICompositionSurface CreateSurface(Compositor compositor)
+        {
+            return CanvasComposition.CreateCompositionSurfaceForSwapChain(compositor, _swapChain);
+        }
+
+        public void Dispose()
+        {
+            lock (_closeLock)
+            {
+                _session?.Dispose();
+                _framePool?.Dispose();
+                _swapChain?.Dispose();
+
+                _swapChain = null;
+                _framePool = null;
+                _session = null;
+                _item = null;
+            }
+        }
+
+        private void OnFrameArrived(Direct3D11CaptureFramePool sender, object args)
+        {
+            lock (_closeLock)
+            {
+                var newSize = false;
+
+                using (var frame = sender.TryGetNextFrame())
+                {
+                    if (frame.ContentSize.Width != _lastSize.Width ||
+                        frame.ContentSize.Height != _lastSize.Height)
+                    {
+                        // The thing we have been capturing has changed size.
+                        // We need to resize our swap chain first, then blit the pixels.
+                        // After we do that, retire the frame and then recreate our frame pool.
+                        newSize = true;
+                        _lastSize = frame.ContentSize;
+                        _swapChain.ResizeBuffers(_lastSize.Width, _lastSize.Height);
+                    }
+
+                    using (var bitmap = CanvasBitmap.CreateFromDirect3D11Surface(_device, frame.Surface))
+                    using (var drawingSession = _swapChain.CreateDrawingSession(Colors.Transparent))
+                    {
+                        drawingSession.DrawImage(bitmap);
+                    }
+
+                } // retire the frame
+
+                _swapChain.Present();
+
+                if (newSize)
+                {
+                    _framePool.Recreate(
+                        _device,
+                        DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                        2,
+                        _lastSize);
+                }
+            }
+        }
+
+        private GraphicsCaptureItem _item;
+        private Direct3D11CaptureFramePool _framePool;
+        private GraphicsCaptureSession _session;
+        private SizeInt32 _lastSize;
+
+        private CanvasDevice _device;
+        private CanvasSwapChain _swapChain;
+
+        private object _closeLock;
+    }
+}
